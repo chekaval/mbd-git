7b5e7beeef0225e8d0f36d8c195352253861ca41
Valeriia Chekanova <v.chekanova@student.utwente.nl>
2023-01-11 10:19:34 +0100

    add file for the 2nd question

---
+++ location-language.py
@@ -0,0 +1,2 @@
+# Are there any correlations between a user’s location and the language they use in their repositories?
+# Language here means programming language, maybe people in europe are more likely to use a specific programming language
