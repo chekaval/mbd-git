db423b5660cdb502cba3eddf6df595513f6e8cba
Valeriia Chekanova <v.chekanova@student.utwente.nl>
2023-01-21 21:50:26 +0100

    enhance the results presentation, so the <location | language> is computed correctly

--- location-language.py
+++ location-language.py
@@ -12,17 +12,16 @@ import re
 
 
 # TODO the function does not work TypeError: 'Column' object is not callable, will be fixed if time allows
-# lowercase, take the first word and remove symbols
-# def normalize_location(string):
-#     lower_string = string.lower()
-#     words = lower_string.split()
-#     if len(words[0]) < 2:
-#         word = words[0] + ' ' + words[1]
-#     else:
-#         word = words[0]
-#     return re.sub('[^a-zA-Z]+', '', word)
 
-
+# lowercase, take the first word and remove symbols except a whitespace
+# if the first word is new or san keep first and second words, otherwise only thr first one
+def normalize_location(string):
+    lower_string = string.lower()
+    words = lower_string.split()
+    if words[0] == "san" or words[0].lower() == "new":
+        return " ".join(string.split()[:2])
+    else:
+        return " ".join(string.split()[:1])
 
 
 # PATH = '/user/s2334232/github.json'  # the whole dataset as one file (using partitions is preferred)
@@ -32,13 +31,36 @@ PATH_DEBUG = '/user/s2334232/project/part-00037-4e9879c8-68e5-4a6b-aac0-f39462b3
 ss = sql.SparkSession.builder.getOrCreate()
 
 df1 = ss.read.json(PATH_DEBUG)
-# df2 = df1.sample(.001)  # DEBUG: Take a small fraction of the dataset
-# df2.printSchema()  # DEBUG
 
 df3 = df1.select(regexp_replace(split(lower(col('location')), ' ')[0], "[^a-zA-Z0-9 ]", "").alias('location'),
+                 # TODO it's not clear |         |       Java|  134| why do we have an empty location like this,
+                 # TODO what symbols does it contain?
                  explode((col('repo_list')['language'])).alias('language'),
-                 col('login').alias('login')).dropDuplicates()
-df3.show()
+                 col('login').alias('login'))
+# TODO check that is not counted as | aus | 2 |
+# |     aus|       PHP|    adrivar0909|
+# |     aus|       PHP|    adrivar0909|
+#     # .dropDuplicates(["language", "login"]) <- not sure if it workds
 df4 = df3.filter(col("location").isNotNull() & col("language").isNotNull()). \
-    groupby('location').count().sort(desc('count')).filter("count > 1")
+    groupby(['location', 'language']).count().sort(desc('count')).filter("count > 1")
+
+# +---------+-----------+-----+
+# | location|   language|count|
+# +---------+-----------+-----+
+# |   austin|       Ruby|  305|
+# |  rosario| JavaScript|  286|
+# |   tehran|      Swift|  254|
+# |      san| JavaScript|  206|
+# | enumclaw|       Ruby|  195|
+# |  rosario|        PHP|  188|
+# |      new| JavaScript|  169|
+# |    paris| JavaScript|  166|
+# | waterloo|        C++|  144|
+# |   united|       Java|  140|
+# |         |       Java|  134|
+
+# TODO more often than not we get cities in the first column.
+# TODO would be nice to replace it by the contry name and then count it.
+# TODO so Retiro (Argentina) | Java | 20 and Quilmes (Argentina) | Java | 30 results in Argentina | Java | 50
+
 df4.show()
