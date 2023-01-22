8c6f51712a1a1015b3202919b3a1c9bf2540040f
h4nek <42340131+h4nek@users.noreply.github.com>
2023-01-22 10:01:37 +0100

    cleanup

--- commit-trends.py
+++ commit-trends.py
@@ -35,7 +35,6 @@ compute_days_diff_udf = udf(lambda str_create_date, str_dates_arr: [(to_date(str
                                                                     for str_date in str_dates_arr], ArrayType(IntegerType()))
 commit_days_dict_udf = udf(lambda days_arr, users_count: to_count_dict(days_arr, users_count),
                            MapType(IntegerType(), DoubleType(), False))
-# commit_date_debug_udf = udf(lambda arr: [e['generate_at'] for e in arr], ArrayType(StringType()))
 
 ss = sql.SparkSession.builder.getOrCreate()
 
@@ -47,9 +46,6 @@ print("number of data rows:", df2.count())  # DEBUG
 df3 = df2.select(col('id'), col('created_at'), col('commit_list'))
 df3.withColumn('First_Commit', df3.commit_list.getItem(0)).show()
 df4 = df3.filter(size(col('commit_list')) > 0)  # exclude users with no commits
-# df4 = df3.withColumn('Commit_Dates', commit_date_udf(col('commit_list')))
-# df5 = df4.select(col('id'), user_id_udf(col('commit_list')).alias('User_ID'),
-#                  commit_date_udf(col('commit_list')).alias('Commit_Date'))
 df5 = df4.withColumn('commit_list', commit_date_udf(col('commit_list')))\
          .withColumnRenamed('commit_list', 'commit_dates')
 df6 = df5.withColumn('commit_dates', extract_date_udf(col('commit_dates')))  # extract the dates (leave out times)
@@ -57,9 +53,6 @@ df7 = df6.withColumn('created_at', udf(lambda date: date.split()[0])(col('create
 df8 = df7.withColumn('commit_dates', compute_days_diff_udf(col('created_at'), col('commit_dates')))\
     .withColumnRenamed('commit_dates', 'commit_days_since')
 df9 = df8.withColumn('commit_days_since', sort_array(col('commit_days_since')))
-
-# dftest = df7.withColumn('commit_dates', compute_days_diff_debug_udf(col('created_at'), col('commit_dates')))\
-#     .withColumnRenamed('commit_dates', 'commit_days_since')
 
 # TODO make a dictionary of counts of commit_days_since (for each row?), then merge for all users, divide by count
 #  (make an average)
@@ -91,3 +84,4 @@ result = df12.collect()
 #  but it might also be dated before the user account creation (local commits using git?)...
 #  / We can ofc have negative values on the X-axis ('-5 days since the account creation')
 
+# TODO run on the cluster, make a graph from the whole dataset, measure times, try different configs?
